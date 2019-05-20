feba72c6c232e3dc76eb930c03b501c16c3f4b87
Tseipii89 <juho.pouttu@gmail.com>
2019-05-20 21:50:04 +0300

    add delete to pictures

--- application/pictures/views.py
+++ application/pictures/views.py
@@ -1,10 +1,24 @@
 from application import app, db
-from flask import render_template, request
+from flask import redirect, render_template, request, url_for
 from application.pictures.models import Picture
+
+
+@app.route("/pictures", methods=["GET"])
+def pictures_index():
+    return render_template("pictures/list.html", pictures = Picture.query.all())
 
 @app.route("/pictures/new/")
 def pictures_form():
     return render_template("pictures/new.html")
+
+@app.route("/delete_pictures/<picture_id>/", methods=["POST"])
+def remove_picture(picture_id):
+    
+    pic = Picture.query.get_or_404(picture_id)
+    db.session().delete(pic)
+    db.session().commit()
+
+    return redirect(url_for("pictures_index"))
 
 @app.route("/pictures/", methods=["POST"])
 def pictures_create():
@@ -13,4 +27,4 @@ def pictures_create():
     db.session().add(vastaus)
     db.session().commit()
 
-    return "onnistui"
+    return redirect(url_for("pictures_index"))
