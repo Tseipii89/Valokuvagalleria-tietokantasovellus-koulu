b08f79c39f2d53ff2363a2cb232890ea20c33e07
Tseipii89 <juho.pouttu@gmail.com>
2019-05-28 21:04:32 +0300

    Add register without bcryt

--- application/__init__.py
+++ application/__init__.py
@@ -1,16 +1,10 @@
-# Tuodaan Flask käyttöön
 from flask import Flask
 app = Flask(__name__)
 
-# Tuodaan SQLAlchemy käyttöön
 from flask_sqlalchemy import SQLAlchemy
 
-# Käytetään gallery.db-nimistä SQLite-tietokantaa. Kolme vinoviivaa
-# kertoo, tiedosto sijaitsee tämän sovelluksen tiedostojen kanssa
-# samassa paikassa
+# Käytetään gallery.db-nimistä SQLite-tietokantaa
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///gallery.db"
-
-# Pyydetään SQLAlchemyä tulostamaan kaikki SQL-kyselyt
 app.config["SQLALCHEMY_ECHO"] = True
 
 # Luodaan db-olio, jota käytetään tietokannan käsittelyyn
@@ -19,8 +13,30 @@ db = SQLAlchemy(app)
 # Luetaan kansiosta application tiedoston views sisältö
 from application import views
 
+# Tuodaan pictures tietokanta
 from application.pictures import models
 from application.pictures import views
+
+# Tuodaan auth tietokanta
+
+from application.auth import models
+from application.auth import views
+
+#Kirjautuminen
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please login to use this functionality."
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 
 # Luodaan lopulta tarvittavat tietokantataulut
---
+++ application/auth/views.py
@@ -0,0 +1,46 @@
+from flask import render_template, request, redirect, url_for
+from flask_login import login_user, logout_user
+
+from application import app, db
+from application.auth.models import User
+from application.auth.forms import LoginForm, RegisterForm
+
+@app.route("/auth/login", methods = ["GET", "POST"])
+def auth_login():
+    if request.method == "GET":
+        return render_template("auth/loginform.html", form = LoginForm())
+
+    form = LoginForm(request.form)
+    # mahdolliset validoinnit
+
+    user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
+    if not user:
+        return render_template("auth/loginform.html", form = form,
+                               error = "Väärä käyttäjätunnus tai salasana")
+
+
+    login_user(user)
+    return redirect(url_for("index"))  
+
+@app.route("/auth/logout")
+def auth_logout():
+    logout_user()
+    return redirect(url_for("index")) 
+
+@app.route("/auth/new/")
+def register_form():
+    return render_template("auth/new.html", form = RegisterForm())    
+
+
+@app.route("/auth/", methods=["POST"])
+def register_create():
+    form = RegisterForm(request.form)
+
+    # Otetaan talteen formista saadut tiedot
+    
+    newUser = User(name=form.name.data, username=form.username.data, password=form.password.data)
+
+    db.session().add(newUser)
+    db.session().commit()
+
+    return redirect(url_for("index"))
